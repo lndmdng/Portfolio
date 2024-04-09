3ec93d674bbea15eeb324d7d21fbd3175c97c275
lndmdng <137748533+lndmdng@users.noreply.github.com>
2024-04-09 15:23:13 +0200

    Sales SQL data cleaning and PowerBi Visualisations
    Using SQL to clean and improve data quality. A power BI dashboard was made for the end user to analyze their sales data.

---
+++ DIM_Customer.sql
@@ -0,0 +1,83 @@
+SELECT TOP (1000) [GeographyKey]
+      ,[City]
+      ,[StateProvinceCode]
+      ,[StateProvinceName]
+      ,[CountryRegionCode]
+      ,[EnglishCountryRegionName]
+      ,[SpanishCountryRegionName]
+      ,[FrenchCountryRegionName]
+      ,[PostalCode]
+      ,[SalesTerritoryKey]
+      ,[IpAddressLocator]
+  FROM [AdventureWorksDW2019].[dbo].[DimGeography]
+
+  SELECT TOP (1000) [CustomerKey]
+      ,[GeographyKey]
+      ,[CustomerAlternateKey]
+      ,[Title]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[LastName]
+      ,[NameStyle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Suffix]
+      ,[Gender]
+      ,[EmailAddress]
+      ,[YearlyIncome]
+      ,[TotalChildren]
+      ,[NumberChildrenAtHome]
+      ,[EnglishEducation]
+      ,[SpanishEducation]
+      ,[FrenchEducation]
+      ,[EnglishOccupation]
+      ,[SpanishOccupation]
+      ,[FrenchOccupation]
+      ,[HouseOwnerFlag]
+      ,[NumberCarsOwned]
+      ,[AddressLine1]
+      ,[AddressLine2]
+      ,[Phone]
+      ,[DateFirstPurchase]
+      ,[CommuteDistance]
+  FROM [AdventureWorksDW2019].[dbo].[DimCustomer]
+
+  -- Cleansed DIM_Customers Table --
+SELECT 
+  c.customerkey AS CustomerKey, 
+  --      ,[GeographyKey]
+  --      ,[CustomerAlternateKey]
+  --      ,[Title]
+  c.firstname AS [First Name], 
+  --      ,[MiddleName]
+  c.lastname AS [Last Name], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  -- Combined First and Last Name
+  --      ,[NameStyle]
+  --      ,[BirthDate]
+  --      ,[MaritalStatus]
+  --      ,[Suffix]
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+  --      ,[EmailAddress]
+  --      ,[YearlyIncome]
+  --      ,[TotalChildren]
+  --      ,[NumberChildrenAtHome]
+  --      ,[EnglishEducation]
+  --      ,[SpanishEducation]
+  --      ,[FrenchEducation]
+  --      ,[EnglishOccupation]
+  --      ,[SpanishOccupation]
+  --      ,[FrenchOccupation]
+  --      ,[HouseOwnerFlag]
+  --      ,[NumberCarsOwned]
+  --      ,[AddressLine1]
+  --      ,[AddressLine2]
+  --      ,[Phone]
+  c.datefirstpurchase AS DateFirstPurchase, 
+  --      ,[CommuteDistance]
+  g.city AS [Customer City] -- Joined in Customer City from Geography Table
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC -- Ordered List by CustomerKey
---
+++ DIM_Product.sql
@@ -0,0 +1,99 @@
+SELECT TOP (1000) [ProductKey]
+      ,[ProductAlternateKey]
+      ,[ProductSubcategoryKey]
+      ,[WeightUnitMeasureCode]
+      ,[SizeUnitMeasureCode]
+      ,[EnglishProductName]
+      ,[SpanishProductName]
+      ,[FrenchProductName]
+      ,[StandardCost]
+      ,[FinishedGoodsFlag]
+      ,[Color]
+      ,[SafetyStockLevel]
+      ,[ReorderPoint]
+      ,[ListPrice]
+      ,[Size]
+      ,[SizeRange]
+      ,[Weight]
+      ,[DaysToManufacture]
+      ,[ProductLine]
+      ,[DealerPrice]
+      ,[Class]
+      ,[Style]
+      ,[ModelName]
+      ,[LargePhoto]
+      ,[EnglishDescription]
+      ,[FrenchDescription]
+      ,[ChineseDescription]
+      ,[ArabicDescription]
+      ,[HebrewDescription]
+      ,[ThaiDescription]
+      ,[GermanDescription]
+      ,[JapaneseDescription]
+      ,[TurkishDescription]
+      ,[StartDate]
+      ,[EndDate]
+      ,[Status]
+  FROM [AdventureWorksDW2019].[dbo].[DimProduct]
+
+  SELECT TOP (1000) [ProductSubcategoryKey]
+      ,[ProductSubcategoryAlternateKey]
+      ,[EnglishProductSubcategoryName]
+      ,[SpanishProductSubcategoryName]
+      ,[FrenchProductSubcategoryName]
+      ,[ProductCategoryKey]
+  FROM [AdventureWorksDW2019].[dbo].[DimProductSubcategory]
+
+  SELECT TOP (1000) [ProductCategoryKey]
+      ,[ProductCategoryAlternateKey]
+      ,[EnglishProductCategoryName]
+      ,[SpanishProductCategoryName]
+      ,[FrenchProductCategoryName]
+  FROM [AdventureWorksDW2019].[dbo].[DimProductCategory]
+
+  -- Cleansed DIM_Products Table --
+SELECT 
+  p.[ProductKey], 
+  p.[ProductAlternateKey] AS ProductItemCode, 
+  --      ,[ProductSubcategoryKey], 
+  --      ,[WeightUnitMeasureCode]
+  --      ,[SizeUnitMeasureCode] 
+  p.[EnglishProductName] AS [Product Name], 
+  ps.EnglishProductSubcategoryName AS [Sub Category], -- Joined in from Sub Category Table
+  pc.EnglishProductCategoryName AS [Product Category], -- Joined in from Category Table
+  --      ,[SpanishProductName]
+  --      ,[FrenchProductName]
+  --      ,[StandardCost]
+  --      ,[FinishedGoodsFlag] 
+  p.[Color] AS [Product Color], 
+  --      ,[SafetyStockLevel]
+  --      ,[ReorderPoint]
+  --      ,[ListPrice] 
+  p.[Size] AS [Product Size], 
+  --      ,[SizeRange]
+  --      ,[Weight]
+  --      ,[DaysToManufacture]
+  p.[ProductLine] AS [Product Line], 
+  --     ,[DealerPrice]
+  --      ,[Class]
+  --      ,[Style] 
+  p.[ModelName] AS [Product Model Name], 
+  --      ,[LargePhoto]
+  p.[EnglishDescription] AS [Product Description], 
+  --      ,[FrenchDescription]
+  --      ,[ChineseDescription]
+  --      ,[ArabicDescription]
+  --      ,[HebrewDescription]
+  --      ,[ThaiDescription]
+  --      ,[GermanDescription]
+  --      ,[JapaneseDescription]
+  --      ,[TurkishDescription]
+  --      ,[StartDate], 
+  --      ,[EndDate], 
+  ISNULL (p.Status, 'Outdated') AS [Product Status] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimProduct] as p
+  LEFT JOIN dbo.DimProductSubcategory AS ps ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey 
+  LEFT JOIN dbo.DimProductCategory AS pc ON ps.ProductCategoryKey = pc.ProductCategoryKey 
+order by 
+  p.ProductKey asc
